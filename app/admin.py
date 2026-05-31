ae8c1cfc2638fff8d1347c3b224c3105aa495659
casper <casper>
2026-05-31 19:11:01 +0200

    feat: added models for hscode thus file, category and hscode

--- app/admin.py
+++ app/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from app.models import User
+from app.models import User, HsCode
 
 admin.site.register(User)
+admin.site.register(HsCode)
--- app/models.py
+++ app/models.py
@@ -1,8 +1,42 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
+# from django.contrib.postgres.indexes import GinIndex
+# from django.contrib.postgres.operations import TrigramExtension
+
 
 class User(AbstractUser):
     role = models.CharField(
         max_length=5, choices=[("Admin", "admin"), ("Staff", "staff")], default="admin"
     )
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=25)
+
+
+class HsCodeFile(models.Model):
+    hs_code_file = models.FileField(upload_to="hs_code")
+
+
+class HsCode(models.Model):
+    hs_code_file = models.ForeignKey(HsCodeFile, on_delete=models.CASCADE)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    hs_code = models.CharField(max_length=20)
+    description = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+
+
+# class Meta:
+#     indexes = [
+#         GinIndex(
+#             fields=["hs_code"],
+#             opclasses=["gin_trgm_ops"],
+#             name="hscode_hs_code_trgm_idx"
+#         ),
+#         GinIndex(
+#             fields=["description"],
+#             opclasses=["gin_trgm_ops"],
+#             name="hscode_description_trgm_idx"
+#         ),
+#     ]
