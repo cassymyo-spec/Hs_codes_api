b6cbf9efea02a76d14893e64015d6ba5746e919b
Casper Moyo <cassymyo@gmail.com>
2026-06-02 12:19:29 +0200

    Use decouple to manage GUNICORN_WORKERS config

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,8 +1,9 @@
 import os
+from decouple import config
 
 bind = "0.0.0.0:8001"
 
-workers = int(os.getenv("GUNICORN_WORKERS", "3"))
+workers = int(config("GUNICORN_WORKERS", "3"))
 
 timeout = 120
 
